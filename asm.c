05a7454f27006ec91180009b54b131b946154219
Daniel <danieltan1517@protonmail.com>
2021-08-11 05:39:13 -0700

    jai assembly language, delete some intrinsics.

--- asm.c
+++ asm.c
@@ -1,15 +1,5 @@
 #include <stdint.h>
-int64_t trailing_zeros(uint64_t x);
-int64_t leading_zeros(uint64_t x);
 uint64_t bit_reverse64(uint64_t x);
-
-int64_t trailing_zeros(uint64_t x) {
-  return __builtin_ctzll(x);
-}
-
-int64_t leading_zeros(uint64_t x) {
-  return __builtin_clzll(x);
-}
 
 uint64_t bit_reverse64(uint64_t x) {
   return __builtin_bitreverse64(x);
